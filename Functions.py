9f75fef73d905d92f7309f3df36cecb095ec34b6
dkzhang11 <43549684+dkzhang11@users.noreply.github.com>
2018-12-11 16:27:14 -0500

    Menu of features
    Buttons of different features for user to click

---
+++ Functions.py
@@ -0,0 +1,11 @@
+#menue  and buttton features for the program using kivy3
+from kivy.app import App
+#kivy.require("1.8.0")
+from kivy.uix.floatlayout import FloatLayout
+
+class Functions(App):
+    def build(self):
+        return FloatLayout()
+
+if __name__ == "__main__":
+    Functions().run()
